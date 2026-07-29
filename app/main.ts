d601b039c2fdeca66126ff9ceff963c0878144ad
Eli Nelson <eli.n992@stu.nebo.edu>
2026-07-29 14:58:14 -0600

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -9,6 +9,9 @@ const rl = createInterface({
 rl.prompt();
 
 rl.on("line", (command: string) => {
+  if (command === "exit") {
+    process.exit(0);
+  }
   console.log(`${command}: command not found`);
   rl.prompt();
 });
