7743c9f78115c8a2daf2558a1d31e75c2f2a05d2
Eli Nelson <eli.n992@stu.nebo.edu>
2026-07-29 15:20:03 -0600

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,11 +6,13 @@ const rl = createInterface({
   prompt: "$ ",
 });
 
+const builtins = new Set(["echo", "exit", "type"]);
+
 rl.prompt();
 
 rl.on("line", (fullCommand: string) => {
   const [command, ...args] = fullCommand.trim().split(/\s+/);
-  
+  const [target] = args;
 
   if (command === "exit") {
     rl.close();
@@ -22,7 +24,19 @@ rl.on("line", (fullCommand: string) => {
     rl.prompt();
     return;
   }
-  
+
+  if (command === "type") {
+    // TODO: Handle undifined target
+    // TODO: Handle multiple args
+    if (builtins.has(target)) {
+      console.log(`${target} is a shell builtin`);
+    } else {
+      console.log(`${target}: not found`);
+    }
+    rl.prompt();
+    return;
+  }
+
   console.log(`${command}: command not found`);
   rl.prompt();
 });
